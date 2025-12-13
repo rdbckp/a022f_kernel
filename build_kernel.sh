c1bf95bb0ac5d5b07e273e9956e4155d8391ba76
a02-trial <cipin23@gmail.com>
2025-12-13 11:05:23 +0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,16 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 
-export CROSS_COMPILE=$(pwd)/toolchain/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9-lineage-19.1/bin/arm-linux-androidkernel-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
-export CLANG_TRIPLE=arm-linux-gnueabi-
+set -e -x
+
+cd ${GITHUB_WORKSPACE}
+export PATH="${PWD}/toolchain/clang/host/linux-x86/clang-r383902/bin:${PWD}/toolchain/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9-lineage-19.1/bin:${PATH}"
 export ARCH=arm
-export ANDROID_MAJOR_VERSION=r
-
+export CC=clang
+export HOSTCC=clang
+export CLANG_TRIPLE=arm-linux-gnueabi-
+export CROSS_COMPILE=arm-linux-androideabi-
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+rm -rf out
+mkdir -p out
+make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CLANG_TRIPLE=arm-linux-gnueabi- CROSS_COMPILE=arm-linux-androidkernel- clean
+make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CLANG_TRIPLE=arm-linux-gnueabi- CROSS_COMPILE=arm-linux-androidkernel- mrproper
+make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CLANG_TRIPLE=arm-linux-gnueabi- CROSS_COMPILE=arm-linux-androidkernel- a02_defconfig
+make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CLANG_TRIPLE=arm-linux-gnueabi- CROSS_COMPILE=arm-linux-androidkernel- -j16
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a02_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
-
-cp out/arch/arm/boot/Image $(pwd)/arch/arm/boot/Image
-cp out/arch/arm/boot/Image $(pwd)/arch/arm64/boot/Image
+cp out/arch/arm/boot/zImage ${PWD}/arch/arm/boot/zImage
+cp out/arch/arm/boot/Image ${PWD}/arch/arm/boot/Image
