d3c6ce41fd3545ff68e7a188966b0a7f004ab11b
rdbckp <ardibackup@gmail.com>
2025-12-14 22:39:09 +0700

    Update build_kernel.sh
    Signed-off-by: rdbckp <ardibackup@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,7 +2,15 @@
 
 set -e -x
 
-export PATH="${PWD}/toolchain/clang/bin:${PWD}/toolchain/gcc/bin:${PATH}"
+# custom toolchain preparation
+mkdir toolchain2
+cd toolchain2
+if [ ! -d clang ]; then mkdir clang && curl -Lsq "${CLANG_URL}" -o clang.tgz && tar -xzf clang.tgz -C clang; fi
+if [ ! -d gcc ]; then mkdir gcc && curl -Lsq "${GCC_URL}" -o gcc.tgz && tar -xzf gcc.tgz -C gcc; fi
+cd -
+export PATH="${PWD}/toolchain2/clang/bin:${PWD}/toolchain2/gcc/bin:${PATH}"
+
+# export PATH="${PWD}/toolchain/clang/bin:${PWD}/toolchain/gcc/bin:${PATH}"
 export ARCH=arm
 export CC=clang
 export HOSTCC=clang
@@ -17,11 +25,11 @@ make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOST
 make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CLANG_TRIPLE=arm-linux-gnueabi- CROSS_COMPILE=arm-linux-androidkernel- a02_defconfig
 make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CLANG_TRIPLE=arm-linux-gnueabi- CROSS_COMPILE=arm-linux-androidkernel- -j16
 
-cp out/arch/arm/boot/zImage ${PWD}/zImage
-mv zImage boot.img-kernel
+# cp out/arch/arm/boot/zImage ${PWD}/zImage
+# mv zImage boot.img-kernel
 
-git config user.name "rdbckp"
-git config user.email "ardibackup@gmail.com"
-git add boot.img-kernel
-git commit -m "kernel ready"
-git push
+# git config user.name "rdbckp"
+# git config user.email "ardibackup@gmail.com"
+# git add boot.img-kernel
+# git commit -m "kernel ready"
+# git push
