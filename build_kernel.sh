84ba0c76654f3f727016f47e2621929bfe38d335
rdbckp <ardibackup@gmail.com>
2025-12-14 19:48:15 +0700

    Update build_kernel.sh
    Signed-off-by: rdbckp <ardibackup@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -18,3 +18,10 @@ make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOST
 make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CLANG_TRIPLE=arm-linux-gnueabi- CROSS_COMPILE=arm-linux-androidkernel- -j16
 
 cp out/arch/arm/boot/zImage ${PWD}/zImage
+mv zImage boot.img-kernel
+
+git config user.name "rdbckp"
+git config user.email "ardibackup@gmail.com"
+git add boot.img-kernel
+git commit -m "kernel ready"
+git push
