2b330ee603b34b7249d49e364bc697ce29283b60
rdbckp <ardibackup@gmail.com>
2025-12-19 19:15:35 +0700

    Create ba.sh
    Signed-off-by: rdbckp <ardibackup@gmail.com>

---
+++ ba.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -e -x
+
+# custom toolchain preparation
+cd ${GITHUB_WORKSPACE}
+ls
+export PATH="${PWD}/toolchain2/clang/bin:${PWD}/toolchain2/gcc/bin:${PATH}"
+
+# export PATH="${PWD}/toolchain/clang/bin:${PWD}/toolchain/gcc/bin:${PATH}"
+export ARCH=arm
+export CC=clang
+export HOSTCC=clang
+export CLANG_TRIPLE=arm-linux-gnueabi-
+export CROSS_COMPILE=arm-linux-androideabi-
+export KCFLAGS=-w
+export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+rm -rf out
+mkdir -p out
+make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CLANG_TRIPLE=arm-linux-gnueabi- CROSS_COMPILE=arm-linux-androidkernel- clean
+make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CLANG_TRIPLE=arm-linux-gnueabi- CROSS_COMPILE=arm-linux-androidkernel- mrproper
+make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CLANG_TRIPLE=arm-linux-gnueabi- CROSS_COMPILE=arm-linux-androidkernel- a02_defconfig
+make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CLANG_TRIPLE=arm-linux-gnueabi- CROSS_COMPILE=arm-linux-androidkernel- modules
+
+# cp out/arch/arm/boot/zImage ${PWD}/zImage
+# mv zImage boot.img-kernel
+
+# git config user.name "rdbckp"
+# git config user.email "ardibackup@gmail.com"
+# git add boot.img-kernel
+# git commit -m "kernel ready"
+# git push
