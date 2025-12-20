9eab9e8888d21a3f2201528e2b6643f75814db3b
rdbckp <ardibackup@gmail.com>
2025-12-20 15:06:37 +0700

    Update build_kernel.sh
    Signed-off-by: rdbckp <ardibackup@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,17 +4,6 @@ set -e -x
 
 # custom toolchain preparation
 cd ${GITHUB_WORKSPACE}
-ls
-export PATH="${PWD}/toolchain2/clang/bin:${PWD}/toolchain2/gcc/bin:${PATH}"
-
-# export PATH="${PWD}/toolchain/clang/bin:${PWD}/toolchain/gcc/bin:${PATH}"
-export ARCH=arm
-export CC=clang
-export HOSTCC=clang
-export CLANG_TRIPLE=arm-linux-gnueabi-
-export CROSS_COMPILE=arm-linux-androideabi-
-export KCFLAGS=-w
-export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 rm -rf out
 mkdir -p out
 make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CLANG_TRIPLE=arm-linux-gnueabi- CROSS_COMPILE=arm-linux-androidkernel- clean
