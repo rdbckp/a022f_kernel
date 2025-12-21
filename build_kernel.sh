6da1186d12b28fdc7f0d1c1ff0a933e27bb44fa0
rdbckp <ardibackup@gmail.com>
2025-12-21 18:56:35 +0700

    Update build_kernel.sh
    Signed-off-by: rdbckp <ardibackup@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,16 +6,12 @@ set -e -x
 cd ${GITHUB_WORKSPACE}
 export PATH="${PWD}/toolchain2/clang/bin:${PWD}/toolchain2/gcc/bin:${PATH}"
 mkdir -p out
-make O=out ARCH=arm CC=clang HOSTCC=clang CROSS_COMPILE=arm-linux-androideabi- clean
-make O=out ARCH=arm CC=clang HOSTCC=clang CROSS_COMPILE=arm-linux-androideabi- mrproper
-make O=out ARCH=arm CC=clang HOSTCC=clang CROSS_COMPILE=arm-linux-androideabi- a02_defconfig
-make O=out ARCH=arm CC=clang HOSTCC=clang CROSS_COMPILE=arm-linux-androideabi- -j16
+export KCFLAGS=-w
+export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CROSS_COMPILE=arm-linux-androideabi- clean
+make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CROSS_COMPILE=arm-linux-androideabi- mrproper
+make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CROSS_COMPILE=arm-linux-androideabi- a02_defconfig
+make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CROSS_COMPILE=arm-linux-androideabi- -j16
 
 cp out/arch/arm/boot/zImage ${PWD}/zImage
 mv zImage boot.img-kernel
-
-# git config user.name "rdbckp"
-# git config user.email "ardibackup@gmail.com"
-# git add boot.img-kernel
-# git commit -m "kernel ready"
-# git push
