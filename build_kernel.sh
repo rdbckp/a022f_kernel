cf9ae00a11d7d2de8ae8da75b07f74cf87e8b719
rdbckp <ardibackup@gmail.com>
2026-01-03 18:37:48 +0700

    Update build_kernel.sh
    Signed-off-by: rdbckp <ardibackup@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,10 +4,10 @@ set -e -x
 cd ${GITHUB_WORKSPACE}
 
 # custom toolchain preparation
-export PATH="${PWD}/toolchain2/clang/bin:${PWD}/toolchain2/gcc/bin:${PATH}"
+# export PATH="${PWD}/toolchain2/clang/bin:${PWD}/toolchain2/gcc/bin:${PATH}"
 
 # toolchain preparation
-# export PATH="$(pwd)/toolchain/clang/bin:${PWD}/toolchain/gcc/bin:${PATH}"
+export PATH="$(pwd)/toolchain/clang/bin:${PWD}/toolchain/gcc/bin:${PATH}"
 rm -rf out
 mkdir -p out
 
@@ -21,4 +21,4 @@ export CFLAGS_WARN=-Wunused-but-set-variable
 export xxx="KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y CFLAGS_WARN=-Wunused-but-set-variable ARCH=arm CC=clang HOSTCC=clang CROSS_COMPILE=arm-linux-androideabi-"
 
 make O=out $xxx a02_defconfig
-make O=out $xxx -j16 2>&1 | tee build.log
+make O=out $xxx -j"$(nproc --all)" 2>&1 | tee build.log
