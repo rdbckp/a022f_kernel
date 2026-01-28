c06a261c5188b68efdb5e1bc22fdc8277f177ba6
rdbckp <ardibackup@gmail.com>
2026-01-28 22:22:57 +0700

    Update build_kernel.sh
    Signed-off-by: rdbckp <ardibackup@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -11,6 +11,11 @@ export PATH="$(pwd)/toolchain/clang/bin:${PWD}/toolchain/gcc/bin:${PATH}"
 rm -rf out
 mkdir -p out
 
+sed -i 's/msleep(ilits->rst_edge_delay);/msleep(1);/g' drivers/input/touchscreen/ili9881x/ili9881x.c && \
+sed -i 's/usleep_range(15000, 15000);/usleep_range(1000, 1000);/g' drivers/input/touchscreen/ili9881x/ili9881x.c && \
+sed -i 's/usleep_range(5 \* 1000, 5 \* 1000);/usleep_range(500, 500);/g' drivers/input/touchscreen/ili9881x/ili9881x.c && \
+sed -i '/ili_ic_init();/a \    device_init_wakeup(ilits->dev, true);' drivers/input/touchscreen/ili9881x/ili9881x.c
+
 export ARCH=arm
 export CC=clang
 export HOSTCC=clang
@@ -20,5 +25,5 @@ export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 export CFLAGS_WARN=-Wunused-but-set-variable
 export xxx="KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y CFLAGS_WARN=-Wunused-but-set-variable ARCH=arm CC=clang HOSTCC=clang CROSS_COMPILE=arm-linux-androideabi-"
 
-make O=out $xxx a02_00_defconfig
+make O=out $xxx a02_defconfig
 make O=out $xxx -j"$(nproc --all)" 2>&1 | tee build.log
