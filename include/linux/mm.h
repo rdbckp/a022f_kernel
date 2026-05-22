6a8bfdeeb184a7bfd636cb55eb0f21f1024be1c7
rdbckp <ardibackup@gmail.com>
2026-05-22 15:05:59 +0700

    Update mm.h
    Signed-off-by: rdbckp <ardibackup@gmail.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2883,8 +2883,5 @@ extern void record_memsize_reserved(const char *name, phys_addr_t base,
 				    phys_addr_t size, bool nomap,
 				    bool reusable);
 extern void record_memsize_memory_hole(void);
-static inline bool need_memory_boosting(struct pglist_data *pgdat) {
-       // Pastikan definisinya ada di sini atau di file .h ini
-   }
 #endif /* __KERNEL__ */
 #endif /* _LINUX_MM_H */
