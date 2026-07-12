3e3cdd519fc2ae0d507d5a6dd10e619b53aa7298
rdbckp <ardibackup@gmail.com>
2026-07-12 09:25:59 +0700

    Update tee_sanity.c
    Signed-off-by: rdbckp <ardibackup@gmail.com>

--- drivers/misc/mediatek/tee_sanity/tee_sanity.c
+++ drivers/misc/mediatek/tee_sanity/tee_sanity.c
@@ -30,14 +30,12 @@ static u64 boot_to_kernel_ns;
 
 void mtk_set_prefer_bigcore(struct task_struct *current_task)
 {
-#ifdef SCHED_PREFER_BIG
-    	sched_set_cpuprefer(current_task->pid, SCHED_PREFER_BIG);
-#endif
+	sched_set_cpuprefer(current->pid, SCHED_PREFER_BIG);
 }
 
 void mtk_set_task_basic_util(struct task_struct *current_task)
 {
-	set_task_util_min_pct(current_task->pid, TEE_TASK_MIN_UTIL);
+	set_task_util_min_pct(current->pid, TEE_TASK_MIN_UTIL);
 }
 
 static void set_boot_to_kernel_time(void)
